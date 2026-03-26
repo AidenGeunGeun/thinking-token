4cdeb2c637faa4e7e2a7c586f72b33e1a67e8ec6
AidenGeunGeun <eastlandwyvern@gmail.com>
2026-03-26 13:31:22 +0900

    Fix tau2-bench package name: metadata says 'tau2' not 'tau2-bench'

--- scripts/setup_runpod.sh
+++ scripts/setup_runpod.sh
@@ -75,7 +75,7 @@ echo "Python: $(python --version) at $(which python)"
 # --- Install Python dependencies -------------------------------------------
 echo "Installing pinned runtime dependencies..."
 uv pip install \
-  "tau2-bench @ git+https://github.com/sierra-research/tau2-bench.git@$TAU2_BENCH_REF" \
+  "tau2 @ git+https://github.com/sierra-research/tau2-bench.git@$TAU2_BENCH_REF" \
   "litellm==$LITELLM_VERSION" \
   "huggingface-hub==$HUGGINGFACE_HUB_VERSION"
 
